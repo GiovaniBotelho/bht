ab5a59a721e6510b6c4a455d12cf3b12393c0a5c
Rodrigo Marafelli <rodrigomarafelli@ioasys.com.br>
2019-11-07 23:11:21 -0300

    bht_2bits working

--- bht.py
+++ bht.py
@@ -1,23 +1,75 @@
-f = open('traces.txt', 'r')
+def read_file():
+    f = open('traces.txt', 'r')
+    predictions = {}
+    for line in f:
+        linha = line.strip('\n').split(' ')##linha completa
+        predictions[linha[0]] = 1
 
-enderecos = {}
-predicoes = {}
+    f.close()
 
-for line in f:
-    linha = line.strip('\n').split(' ')##linha completa
-    if linha[0] in enderecos: #endereco
-        if predicoes[linha[0]] != enderecos[linha[0]]:
-            if predicoes[linha[0]] == 0:
-                predicoes[linha[0]] = 1
-            else:
-                predicoes[linha[0]] = 0
-    else:
-        enderecos[linha[0]] = linha[1]
+    return predictions
+
+def bht_1bit():
+    predictions = read_file()
+
+    nf = open('traces.txt', 'r')
+    errors = 0
+    for line in nf:
+        linha = line.strip('\n').split(' ')##linha completa
+        if predictions[linha[0]] != int(linha[1]):
+            errors += 1
+            predictions[linha[0]] = int(linha[1])
+    
+    nf.close()
+    
+    return [predictions, errors]
+#fortemente tomado 1
+#fracamente tomado 2
+#fortemente no tomado 3
+#fracamente no tomado 0
+
+def bht_2bits():
+    predictions = read_file()
+
+    nf = open('traces.txt', 'r')
+    errors = 0
+    prediction_history = {}
+    for line in nf:
+        linha = line.strip('\n').split(' ')##linha completa7
+        if int(linha[1]) == 1:
+            if predictions[linha[0]] == 2:
+                predictions[linha[0]] = 1
+                # prediction_history[linha[0]].extend(1)
+            elif predictions[linha[0]] == 3:
+                predictions[linha[0]] = 0
+                # prediction_history[linha[0]].push(0) 
+            elif predictions[linha[0]] == 0:
+                errors += 1
+                predictions[linha[0]] = 1
+                # prediction_history[linha[0]].push(1) 
+        else:
+            if predictions[linha[0]] == 1:
+                predictions[linha[0]] = 2
+                # prediction_history[linha[0]].push(2) 
+
+            elif predictions[linha[0]] == 2:
+                errors += 1
+                predictions[linha[0]] = 3
+                # prediction_history[linha[0]].push(3) 
+            elif predictions[linha[0]] == 0:
+                predictions[linha[0]] = 3
+                # prediction_history[linha[0]].push(3) 
+    nf.close()
+    
+    return [predictions, errors, prediction_history]
+            
 
 
+def main():
+    predictions, errors, prediction_history = bht_2bits()
+    print(prediction_history)
 
-
-
-        
+if __name__ == "__main__":
+    main()
 
 
