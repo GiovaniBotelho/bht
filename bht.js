094cf8fb6579493259de9b094b0c90d2b73ce4ae
Mauricio Vieira <mauriciomvdr@hotmail.com>
2019-12-05 00:39:10 -0300

    etapa 1 e etapa 2 concluidas :hammer:

--- bht.js
+++ bht.js
@@ -1,10 +1,70 @@
 var file = null;
-var address = [];
-var predictions = new Map();
-var initial_prediction = 1;
+const TOMADO = 1;
+const TOMADO_FRACAMENTE = 2;
+const NAO_TOMADO = 0;
+const NAO_TOMADO_FRACAMENTE = 3;
+var predictions = [];
+var initial_prediction = TOMADO;
 var index = 0;
-var erros = 0;
 var TIPO_PREDITOR = 0; // 1 -> 1 BIT  e 2 => 2 BIT
+var m = undefined; //tamanho da tabela
+
+function setM() {
+  var num = document.getElementById("m");
+  var option = num.value;
+  m = option;
+  num.disabled = true;
+  var fileSelector = document.getElementById("inputfile");
+  fileSelector.disabled = false;
+}
+
+function disable_button() {
+  var btn1 = document.getElementById("btn1")
+  var btn2 = document.getElementById("btn2")
+  btn1.disabled = true;
+  btn2.disabled = true;
+}
+
+function hexToBinario(num) {
+  //Função para converter hexadecimal em binario já descartando os dois ultimos bits
+  let decimal = parseInt(num, 16);
+  let binario = decimal.toString(2);
+  return binario.slice(0, binario.length - 2);
+}
+
+function binToInteger(num) {
+  var tamanhoTabela = Math.pow(2, m);
+  // var numBits = Math.log2(m); //definindo o numero de bits que sera usado do endereço
+  var inteiro = num.slice(num.length - m);
+  return parseInt(inteiro, 2);
+}
+
+function drawTable() {
+  var attr = document.getElementById("preditores");
+
+  var data =
+  "<table style=width:70% align=center> <caption><h1> Preditores </h1></caption> " +
+  "<tr> <th><h2>Predito</h2></th> <th><h2>Realizado</h2></th> <th><h2>Acerto</h2></th> <th><h2>Erro</h2></th> <th><h2>Precisao</h2></th> </tr>";
+
+  for (var j = 0; j < predictions.length; j++) {
+    if (predictions[j] != undefined) {
+      data += `<tr align=center> <td> ${
+        predictions[j].predicao == TOMADO
+          ? "TOMADO"
+          : predictions[j].predicao == TOMADO_FRACAMENTE
+          ? "TOMADO FRACAMENTE"
+          : predictions[j].predicao == NAO_TOMADO
+          ? "NÃO TOMADO"
+          : "NAO TOMADO FRACAMENTE"
+      } </td> <td> ${
+        predictions[j].realizado == TOMADO ? "TOMADO" : "NÃO TOMADO"
+      } </td> <td> ${predictions[j].acerto} </td> <td> ${
+        predictions[j].erro
+      } </td> <td> ${predictions[j].precisao.toFixed(2)}% </td> </tr>`;
+    }
+  }
+  attr.innerHTML = data + "</table>";
+}
 
 function lerArquivo(files) {
   var atributos = document.getElementById("registros");
@@ -18,95 +78,56 @@ function lerArquivo(files) {
     atributos.innerText = "O arquivo possui " + file.length + " endereços";
     for (var i = 0; i < file.length; i++) {
       line = file[i].split(" "); //linha completa
-      if (!predictions.has(line[0])) {
-        predictions.set(line[0], initial_prediction);
-        address.push(line[0]);
-      }
+      let index = binToInteger(hexToBinario(line[0]));
+      predictions[index] = {
+        predicao: initial_prediction,
+        realizado: -1,
+        acerto: 0,
+        erro: 0,
+        precisao: 0
+      };
     }
+    drawTable();
   };
 }
 
-function bht_1bit_nextStep() {
-  if (file == null || index >= file.length)
-    alert("Arquivo não carregado ou o fim já foi alcançado");
+function bht_1bit_nextStep(draw = true) {
+  if (file == null || index >= file.length || m == undefined)
+    alert(
+      "Arquivo não carregado, ou o fim já foi alcançado ou o parametro m não foi passado"
+    );
   else {
-    console.log(index);
     var line = file[index].split(" ");
-    console.log(file[index].split(" "));
+    var indexPreditor = binToInteger(hexToBinario(line[0]));
 
-    var attr = document.getElementById("predicoes");
-    var incherto = "";
-    incherto =
-      '<table style="width:50%"> <caption> Preditores </caption> ' +
-      "<tr> <th>Index</th> <th>Address</th> <th>Prediction</th> </tr>";
+    predictions[indexPreditor].realizado = line[1];
 
-    for (var j = 0; j <= address.length; j++) {
-      if (line[0] == address[j])
-        incherto +=
-          '<tr style="background-color: #00ff00"> <td>' +
-          j +
-          "</td> <td>" +
-          address[j] +
-          "</td> <td>" +
-          `${predictions.get(address[j])}` +
-          "</td> </tr>";
-      else
-        incherto +=
-          "<tr> <td>" +
-          j +
-          "</td> <td>" +
-          address[j] +
-          "</td> <td>" +
-          predictions.get(address[j]) +
-          "</td> </tr>";
+    if (predictions[indexPreditor].predicao == TOMADO) {
+      if (line[1] == TOMADO) {
+        predictions[indexPreditor].acerto =
+          predictions[indexPreditor].acerto + 1;
+        if (draw) drawTable();
+      } else if (line[1] == NAO_TOMADO) {
+        predictions[indexPreditor].erro = predictions[indexPreditor].erro + 1;
+        if (draw) drawTable();
+        predictions[indexPreditor].predicao = NAO_TOMADO;
+      }
+    } else if (predictions[indexPreditor].predicao == NAO_TOMADO) {
+      if (line[1] == NAO_TOMADO) {
+        predictions[indexPreditor].acerto =
+          predictions[indexPreditor].acerto + 1;
+        if (draw) drawTable();
+      } else if (line[1] == TOMADO) {
+        predictions[indexPreditor].erro = predictions[indexPreditor].erro + 1;
+        if (draw) drawTable();
+        predictions[indexPreditor].predicao = TOMADO;
+      }
     }
-    attr.innerHTML = incherto + "</table>";
-
-    var endereco = document.getElementById("enderecos");
-    var incherto2 =
-      '<table style="width:50%"> <caption> Endereços no arquivo </caption> ' +
-      "<tr> <th>Index</th> <th>Address</th> <th>Prediction</th> <th> </th> </tr>";
-    for (var k = 0; k < file.length; k++) {
-      let line = file[k].split(" ");
-      if (index == k) {
-        incherto2 +=
-          '<tr style="background-color: #00ff00"> <td>' +
-          k +
-          "</td> <td>" +
-          line[0] +
-          "</td> <td>" +
-          line[1] +
-          "</td> <td> <button onClick='bht_1bit_nextStep()'>Próximo passo</button> </td> </tr>";
-        jaPassou = true;
-      } else
-        incherto2 +=
-          "<tr> <td>" +
-          k +
-          "</td> <td>" +
-          line[0] +
-          "</td> <td>" +
-          line[1] +
-          "</td> </tr>";
-    }
-    endereco.innerHTML = incherto2 + "</table>";
-
-    if (!(line[1] == predictions.get(line[0]))) {
-      console.log("errei");
-      erros++;
-      predictions.set(line[0], line[1]);
-    }
+    predictions[indexPreditor].precisao =
+      (predictions[indexPreditor].acerto /
+        (predictions[indexPreditor].acerto + predictions[indexPreditor].erro)) *
+      100;
     index++;
-    if (index >= file.length) {
-      var atributos = document.getElementById("registros");
-      atributos.innerText =
-        "O arquivo possui " +
-        file.length +
-        " endereços => " +
-        Math.trunc(((file.length - erros) / file.length) * 100) +
-        "% de acertos e " +
-        erros +
-        " prediçoes incorretas";
-    }
   }
 }
 
@@ -114,217 +135,107 @@ function bht_1bit_summary() {
   if (file == null || index >= file.length)
     alert("Arquivo não carregado ou o fim já foi alcançado");
   else {
-    console.log(file);
     for (var i = index; i < file.length; i++) {
-      var line = file[i].split(" ");
-      if (!(line[1] == predictions.get(line[0]))) {
-        console.log("errei");
-        erros++;
-        predictions.set(line[0], line[1]);
-      }
-      index++;
+      bht_1bit_nextStep(false);
     }
-
-    var attr = document.getElementById("predicoes");
-    var incherto = "";
-    incherto =
-      "<table> <caption> Preditores </caption>" +
-      '<tr> <th style="width:13%">Index</th> <th style="width:44%">Address</th> <th style="width:43%">Prediction</th> </tr>';
-    console.log(address);
-    for (var j = 0; j < address.length; j++) {
-      incherto +=
-        '<tr> <td style="text-align: center;">' +
-        j +
-        '</td> <td style="text-align: center;">' +
-        address[j] +
-        '</td> <td style="text-align: center;">' +
-        predictions.get(address[j]) +
-        "</td> </tr>";
-    }
-    attr.innerHTML = incherto + "</table>";
-
-    var endereco = document.getElementById("enderecos");
-    var incherto2 =
-      "<table> <caption> Endereços no arquivo </caption> " +
-      '<tr> <th style="width:13%">Index</th> <th style="width:44%">Address</th> <th style="width:43%">Prediction</th </tr>';
-    for (var k = 0; k < file.length; k++) {
-      let line = file[k].split(" ");
-      incherto2 +=
-        '<tr> <td style="text-align: center;">' +
-        k +
-        '</td> <td style="text-align: center;">' +
-        line[0] +
-        '</td> <td style="text-align: center;">' +
-        line[1] +
-        "</td> </tr>";
-    }
-    endereco.innerHTML = incherto2 + "</table>";
-
-    if (index >= file.length) {
-      var atributos = document.getElementById("registros");
-      atributos.innerText =
-        "O arquivo possui " +
-        file.length +
-        " endereços => " +
-        Math.trunc(((file.length - erros) / file.length) * 100) +
-        "% de acertos e " +
-        erros +
-        " prediçoes incorretas";
-    }
+    drawTable();
   }
 }
 
-function bht_2bits_nextStep() {
-  // fortemente tomado 1
-  // fracamente tomado 2
-  // fortemente no tomado 3
-  // fracamente no tomado 0
+function bht_2bits_nextStep(draw = true) {
+  // const TOMADO = 1;
+  // const TOMADO_FRACAMENTO = 2
+  // const NAO_TOMADO = 0;
+  // const NAO_TOMADO_FRACAMENTE = 3
 
-  if (file == null || index >= file.length) {
-    alert("Arquivo não carregado ou o fim já foi alcançado");
-  } else {
+  if (file == null || index >= file.length || m == undefined)
+    alert(
+      "Arquivo não carregado, ou o fim já foi alcançado ou o parametro m não foi passado"
+    );
+  else {
     var line = file[index].split(" ");
+    var indexPreditor = binToInteger(hexToBinario(line[0]));
 
-    var attr = document.getElementById("predicoes");
-    var incherto =
-      '<table> <caption> Preditores </caption> <tr> <th style="width:13%">Index</th> <th style="width:44%">Address</th> <th style="width:43%">Prediction</th> </tr>';
-    for (var j = 0; j < address.length; j++) {
-      console.log(line[0]);
-      console.log(address[j]);
-      if (line[0] == address[j]) {
-        if (predictions.get(line[0]) == 0)
-          incherto +=
-            '<tr> <td style="text-align:center;background-color: #FFDEAD">' +
-            j +
-            '</td> <td style="text-align: center;background-color: #FFDEAD">' +
-            address[j] +
-            '</td> <td style="text-align: center;background-color: #FFDEAD">' +
-            predictions.get(address[j]) +
-            "</td> </tr>";
-        else if (predictions.get(line[0]) == 1)
-          incherto +=
-            '<tr> <td style="text-align:center;background-color: #1E90FF">' +
-            j +
-            '</td> <td style="text-align: center;background-color: #1E90FF">' +
-            address[j] +
-            '</td> <td style="text-align: center;background-color: #1E90FF">' +
-            predictions.get(address[j]) +
-            "</td> </tr>";
-        else if (predictions.get(line[0]) == 2)
-          incherto +=
-            '<tr> <td style="text-align:center;background-color: #B0C4DE">' +
-            j +
-            '</td> <td style="text-align: center;background-color: #B0C4DE">' +
-            address[j] +
-            '</td> <td style="text-align: center;background-color: #B0C4DE">' +
-            predictions.get(address[j]) +
-            "</td> </tr>";
-        else
-          incherto +=
-            '<tr> <td style="text-align: center;background-color: #D2691E">' +
-            j +
-            '</td> <td style="text-align: center;background-color: #D2691E"">' +
-            address[j] +
-            '</td> <td style="text-align: center;background-color: #D2691E"">' +
-            predictions.get(address[j]) +
-            "</td> </tr>";
-      } else {
-        incherto +=
-          '<tr> <td style="text-align: center;">' +
-          j +
-          '</td> <td style="text-align: center;">' +
-          address[j] +
-          '</td> <td style="text-align: center;">' +
-          predictions.get(address[j]) +
-          "</td> </tr>";
-      }
+    predictions[indexPreditor].realizado = line[1];
+    //começando a predição
+    switch (predictions[indexPreditor].predicao) {
+      case TOMADO:
+        if (line[1] == TOMADO) {
+          predictions[indexPreditor].acerto =
+            predictions[indexPreditor].acerto + 1;
+          predictions[indexPreditor].realizado = TOMADO;
+          if (draw) drawTable();
+          predictions[indexPreditor].predicao = TOMADO;
+        } else if (line[1] == NAO_TOMADO) {
+          predictions[indexPreditor].erro = predictions[indexPreditor].erro + 1;
+          predictions[indexPreditor].realizado = NAO_TOMADO;
+          if (draw) drawTable();
+          predictions[indexPreditor].predicao = TOMADO_FRACAMENTE;
+        }
+        break;
+      case TOMADO_FRACAMENTE:
+        if (line[1] == TOMADO) {
+          predictions[indexPreditor].acerto =
+            predictions[indexPreditor].acerto + 1;
+          predictions[indexPreditor].realizado = TOMADO;
+          if (draw) drawTable();
+          predictions[indexPreditor].predicao = TOMADO;
+        } else if (line[1] == NAO_TOMADO) {
+          predictions[indexPreditor].erro = predictions[indexPreditor].erro + 1;
+          predictions[indexPreditor].realizado = NAO_TOMADO;
+          if (draw) drawTable();
+          predictions[indexPreditor].predicao = NAO_TOMADO;
+        }
+        break;
+      case NAO_TOMADO:
+        if (line[1] == TOMADO) {
+          predictions[indexPreditor].erro = predictions[indexPreditor].erro + 1;
+          predictions[indexPreditor].realizado = TOMADO;
+          if (draw) drawTable();
+          predictions[indexPreditor].predicao = NAO_TOMADO_FRACAMENTE;
+        } else if (line[1] == NAO_TOMADO) {
+          predictions[indexPreditor].acerto =
+            predictions[indexPreditor].acerto + 1;
+          predictions[indexPreditor].realizado = NAO_TOMADO;
+          if (draw) drawTable();
+          predictions[indexPreditor].predicao = NAO_TOMADO;
+        }
+        break;
+      case NAO_TOMADO_FRACAMENTE:
+        if (line[1] == TOMADO) {
+          predictions[indexPreditor].erro = predictions[indexPreditor].erro + 1;
+          predictions[indexPreditor].realizado = TOMADO;
+          if (draw) drawTable();
+          predictions[indexPreditor].predicao = TOMADO;
+        } else if (line[1] == NAO_TOMADO) {
+          predictions[indexPreditor].acerto =
+            predictions[indexPreditor].acerto + 1;
+          predictions[indexPreditor].realizado = NAO_TOMADO;
+          if (draw) drawTable();
+          predictions[indexPreditor].predicao = NAO_TOMADO;
+        }
+        break;
+      default:
+        alert("Deu alguma merda no código");
     }
-    attr.innerHTML = incherto + "</table>";
 
-    var endereco = document.getElementById("enderecos");
-    var incherto2 =
-      '<table> <caption> Endereços no arquivo </caption> <tr> <th style="width:13%">Index</th> <th style="width:44%">Address</th> <th style="width:43%">Prediction</th </tr>';
-    for (var k = 0; k < file.length; k++) {
-      let line = file[k].split(" ");
-      if (index == k) {
-        incherto2 +=
-          '<tr style="background-color: #00ff00"> <td style="text-align: center;">' +
-          k +
-          '</td> <td style="text-align: center;">' +
-          line[0] +
-          '</td> <td style="text-align: center;">' +
-          line[1] +
-          "</td> </tr>";
-      } else {
-        incherto2 +=
-          '<tr> <td style="text-align: center;">' +
-          k +
-          '</td> <td style="text-align: center;">' +
-          line[0] +
-          '</td> <td style="text-align: center;">' +
-          line[1] +
-          "</td> </tr>";
-      }
-    }
-    endereco.innerHTML = incherto2 + "</table>";
-
-    if (line[1] == 1) {
-      switch (predictions.get(line[0])) {
-        case 2:
-          predictions.set(line[0], 1);
-          break;
-        case 3:
-          predictions.set(line[0], 0);
-          break;
-        case 0:
-          predictions.set(line[0], 1);
-          erros++; // Estava no fracamente não tomado e fui para tomado
-          break;
-      }
-    } else if (line[1] == 0) {
-      switch (predictions.get(line[0])) {
-        case 0:
-          predictions.set(line[0], 3);
-          break;
-        case 1:
-          predictions.set(line[0], 2);
-          break;
-        case 2:
-          predictions.set(line[0], 3);
-          console.log("errei");
-          erros++; // estava no framente tomado e fui para não tomado
-          break;
-      }
-    }
+    predictions[indexPreditor].precisao =
+      (predictions[indexPreditor].acerto /
+        (predictions[indexPreditor].acerto + predictions[indexPreditor].erro)) *
+      100;
     index++;
-    if (index >= file.length) {
-      var atributos = document.getElementById("registros");
-      atributos.innerText =
-        "O arquivo possui " +
-        file.length +
-        " endereços => " +
-        Math.trunc(((file.length - erros) / file.length) * 100) +
-        "% de acertos e " +
-        erros +
-        " prediçoes incorretas";
-    }
   }
 }
 
 function bht_2bits_summary() {
-  for (var i = index; i < file.length; i++) {
-    bht_2bits_nextStep();
+  if (file == null || index >= file.length)
+    alert("Arquivo não carregado ou o fim já foi alcançado");
+  else {
+    for (var i = index; i < file.length; i++) {
+      bht_2bits_nextStep(false);
+    }
+    drawTable();
   }
-
-  var atributos = document.getElementById("registros");
-  atributos.innerText =
-    "O arquivo possui " +
-    file.length +
-    " endereços => " +
-    Math.trunc(((file.length - erros) / file.length) * 100) +
-    "% de acertos e " +
-    erros +
-    " prediçoes incorretas";
 }
 
 function bht_nextStep() {
@@ -337,6 +248,7 @@ function bht_summary() {
   if (TIPO_PREDITOR == 0) alert("Nenhum preditor selecionado!");
   else if (TIPO_PREDITOR == 1) bht_1bit_summary();
   else bht_2bits_summary();
+  disable_button()
 }
 
 function selecionaPreditor() {
@@ -344,17 +256,4 @@ function selecionaPreditor() {
   var option = select.options[select.selectedIndex].value;
   TIPO_PREDITOR = option;
   select.disabled = true;
-  if (TIPO_PREDITOR == 1) {
-    var legenda = document.getElementById("legenda");
-    legenda.innerHTML = "1 - Tomado |  0 - Não Tomado";
-  } else if (TIPO_PREDITOR == 2) {
-    var legenda = document.getElementById("legenda");
-    legenda.innerHTML =
-      '<div style="width: 100%;">' +
-      '<p style="background-color: #FFDEAD; width: auto; color: #000000;float: left;margin-right: 10px;padding: 5px"> 0 - Fracamento não tomado </p>' +
-      '<p style="background-color: #1E90FF; width: auto; color: #ffffff;float: left;margin-right: 10px;padding: 5px"> 1 - Fortemente tomado </p>' +
-      '<p style="background-color: #B0C4DE; width: auto; color: #ffffff;float: left;margin-right: 10px;padding: 5px"> 2 - Fracamente tomado </p>' +
-      '<p style="background-color: #D2691E; width: auto; color: #ffffff;float: left;margin-right: 10px;padding: 5px"> 3 - Fortemente não tomado </p>' +
-      "</div>";
-  }
 }
---
+++ ght.js
@@ -0,0 +1,180 @@
+var file = null;
+var predictions = [];
+const TOMADO = 1;
+const NAO_TOMADO = 0;
+var index = 0;
+var TIPO_PREDITOR = 0; // 1 -> 1 BIT  e 2 => 2 BIT
+var m = undefined; //tamanho da tabela
+
+function setM() {
+  var num = document.getElementById("m");
+  var option = num.value;
+  m = option;
+  num.disabled = true;
+  var fileSelector = document.getElementById("inputfile");
+  fileSelector.disabled = false;
+}
+
+function disable_button() {
+  var btn1 = document.getElementById("btn1");
+  var btn2 = document.getElementById("btn2");
+  btn1.disabled = true;
+  btn2.disabled = true;
+}
+
+function hexToBinario(num) {
+  //Função para converter hexadecimal em binario já descartando os dois ultimos bits
+  let decimal = parseInt(num, 16);
+  let binario = decimal.toString(2);
+  return binario.slice(0, binario.length - 2);
+}
+
+function binToInteger(num) {
+  var tamanhoTabela = Math.pow(2, m);
+  // var numBits = Math.log2(m); //definindo o numero de bits que sera usado do endereço
+  var inteiro = num.slice(num.length - m);
+  return parseInt(inteiro, 2);
+}
+
+function drawTable() {
+  var attr = document.getElementById("preditores");
+
+  var data =
+    "<table style=width:70% align=center> <caption><h1> Preditores </h1></caption> " +
+    "<tr> <th><h2>Predito</h2></th> <th><h2>Realizado</h2></th> <th><h2>Acerto</h2></th> <th><h2>Erro</h2></th> <th><h2>Precisao</h2></th> </tr>";
+
+  for (var j = 0; j < predictions.length; j++) {
+    if (predictions[j] != undefined) {
+      data += `<tr align=center> <td> ${
+        predictions[j].counter >= 2 ? "TOMADO" : "NÃO TOMADO"
+      } </td> <td> ${
+        predictions[j].realizado == TOMADO ? "TOMADO" : "NÃO TOMADO"
+      } </td> <td> ${predictions[j].acerto} </td> <td> ${
+        predictions[j].erro
+      } </td> <td> ${predictions[j].precisao.toFixed(2)}% </td> </tr>`;
+    }
+  }
+  attr.innerHTML = data + "</table>";
+}
+
+function lerArquivo(files) {
+  var atributos = document.getElementById("registros");
+  var fileIn = files[0];
+  var reader = new FileReader();
+  reader.readAsText(fileIn);
+  reader.onload = function(event) {
+    // console.log(event.target.result);
+    file = event.target.result;
+    file = file.split("\n");
+    atributos.innerText = "O arquivo possui " + file.length + " endereços";
+    for (var i = 0; i < file.length; i++) {
+      line = file[i].split(" "); //linha completa
+      let index = binToInteger(hexToBinario(line[0]));
+      if (predictions[index] == undefined) {
+        predictions[index] = {
+          predictions: [],
+          counter: 2,
+          acerto: 0,
+          erro: 0,
+          precisao: 0
+        };
+      }
+    }
+    drawTable();
+  };
+}
+
+// const TOMADO = 3;
+// const TOMADO_FRACAMENTE = 2;
+// const NAO_TOMADO = 1;
+// const NAO_TOMADO_FRACAMENTE = 0;
+
+function getPrediction(indexPrediction) {
+  var ini = 0;
+
+  if (predictions[indexPrediction].predictions.length > m)
+    ini = predictions[indexPrediction].predictions.length - m;
+
+  console.log(predictions[indexPrediction].counter < 3);
+
+  for (var i = ini; i < predictions[indexPrediction].predictions.length; i++) {
+    console.log(predictions[indexPrediction].predictions[i].realizado);
+    if (predictions[indexPrediction].predictions[i].predicao == TOMADO) {
+      console.log("oi");
+      if (predictions[indexPrediction].counter < 3) {
+        predictions[indexPrediction].counter =
+          predictions[indexPrediction].counter + 1;
+      }
+    } else {
+      console.log("tchau");
+      if (predictions[indexPrediction].counter > 0) {
+        predictions[indexPrediction].counter =
+          predictions[indexPrediction].counter - 1;
+      }
+    }
+  }
+
+  if (predictions[indexPrediction].counter >= 2) return TOMADO;
+
+  if (index == 0) predictions[indexPrediction].counter -= 1;
+  return NAO_TOMADO;
+}
+
+function ght_nextStep(draw = true) {
+  if (file == null || index >= file.length || m == undefined)
+    alert(
+      "Arquivo não carregado, ou o fim já foi alcançado ou o parametro m não foi passado"
+    );
+  else {
+    var line = file[index].split(" ");
+    var indexPreditor = binToInteger(hexToBinario(line[0]));
+
+    if (predictions[indexPreditor].counter >= 2) {
+      //predição foi dada como tomada
+      if (line[1] == TOMADO) {
+        if (predictions[indexPreditor].counter < 3) {
+          predictions[indexPreditor].counter += 1;
+        }
+        predictions[indexPreditor].acerto += 1;
+      } else if (line[1] == NAO_TOMADO) {
+        predictions[indexPreditor].counter -= 1;
+        predictions[indexPreditor].erro += 1;
+      }
+    } else {
+      if (line[1] == NAO_TOMADO) {
+        predictions[indexPreditor].counter += 1;
+        predictions[indexPreditor].acerto += 1;
+      } else if (line[1] == TOMADO) {
+        if (predictions[indexPreditor].counter > 0) {
+          predictions[indexPreditor].counter -= 1;
+        }
+        predictions[indexPreditor].erro += 1;
+      }
+    }
+
+    predictions[indexPreditor].predictions.push({
+      predicao: predictions[indexPreditor].counter >= 2 ? TOMADO : NAO_TOMADO,
+      realizado: parseInt(line[1])
+    });
+
+    predictions[indexPreditor].precisao =
+      (predictions[indexPreditor].acerto /
+        (predictions[indexPreditor].acerto + predictions[indexPreditor].erro)) *
+      100;
+
+    index++;
+    if (draw) drawTable();
+  }
+}
+
+function ght_summary() {
+  if (file == null || index >= file.length || m == undefined)
+    alert("Arquivo não carregado ou o fim já foi alcançado");
+  else {
+    for (var i = index; i < file.length; i++) {
+      ght_nextStep(false);
+    }
+    drawTable();
+  }
+  disable_button();
+}
